63ba988bfe74d4277a8dcdf94da18b56b8557419
School and Tech <School and Tech@ECO-POWER320>
2022-05-09 11:20:29 -0500

    First Update

--- BankManagementProject/Source.cpp
+++ BankManagementProject/Source.cpp
@@ -1,14 +1,20 @@
+#include "main.h"
 #include <iostream>
+
 using std::string;
 
+int tExit() {
+	char shutdown;
+	std::cout << "Would you like to exit? Y - Yes | N - No." << "\n";
+	std::cin >> shutdown;
+	putchar(toupper(shutdown));
 
-
-class userAccount {
-public:
-	string f_Name;
-	string l_Name;
-	int accountNum;
-	int balance;
-
-
-};
+	if (shutdown == 'Y') {
+		std::cout << "Program Exited" << "\n";
+		programExit = true;
+	}
+	else if (shutdown == 'N') {
+		std::cout << "Not Okay" << "\n";
+	}
+	return shutdown;
+}
--- BankManagementProject/main.cpp
+++ BankManagementProject/main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <cstring>
+
 #include "main.h"
 
 
+//class userAccount {
+//public:
+	//string fName;
+	//string lName;
+	//int accountNum;
+	//int balance;
+
+//};
+
+bool programExit = false;
+
 int main() {
 
+		// Keeps the program running whilst the user manipulates
+		// the data.
+	while (programExit == false) {
+
+		else if (userChoice == 3) {
+			
+}
+
+		else if (userChoice == 4) {
+			tExit();
+		}
+		
+	}
 	return 0;
 }
