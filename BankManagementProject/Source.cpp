9b1fc70305fb8309bb15be2cf9ffbd74d0faa1d6
School and Tech <School and Tech@ECO-POWER320>
2022-05-10 20:23:50 -0500

    Completed the "openAcct" function in the source file.

--- BankManagementProject/Source.cpp
+++ BankManagementProject/Source.cpp
@@ -2,18 +2,52 @@
 #include <iostream>
 #include <cstring>
 
-
+std::string fName;
+std::string lName;
+char amountChoice;
+int bankAmount;
+char acctChoice;
+char acctType;
+bool isTrue = false;
 
 void openAcct() {
-	char acctChoice;
-	char acctType;
-	bool isTrue = false;
+
+	std::cout << "What is your first name?: ";
+	std::cin >> fName;
+
+	std::cout << "What is your last name?: ";
+	std::cin >> lName;
+
+	while (isTrue == false) {
+		std::cout << "Would you like to initialize your bank account with a deposit? (Y - Yes | N - No): \n";
+		std::cin >> amountChoice;
+		char user1Ans = toupper(amountChoice);
+
+		if (amountChoice == 'Y') {
+			std::cout << "How much money would you like to deposit?: \n";
+			std::cin >> bankAmount;
+			isTrue = true;
+		}
+		else if (amountChoice == 'N') {
+			isTrue = true;
+			bankAmount = 0;
+			break;
+		}
+		else {
+			std::cout << "Invalid Input. Please try again.\n";
+			std::cout << "\n";
+		}
+
+	}
+
+	isTrue = false;
 
 
 	while (isTrue == false) {
-		std::cout << "What type of bank account would like to open? Checkings - C | Savings - S: ";
+		
+		std::cout << "What type of bank account would like to open? Checkings - C | Savings - S: \n";
 		std::cin >> acctChoice;
-		putchar(toupper(acctChoice));
+		char user2Ans = toupper(acctChoice);
 
 		if (acctChoice == 'C') {
 			acctType = 'C';
@@ -27,11 +61,14 @@ void openAcct() {
 			std::cout << "\nThat was not a valid input. Please try again.\n";
 		}
 	}
+	std::cout << "\n";
+	std::cout << "First name: " << fName << "\n";
+	std::cout << "Last name: " << lName << "\n";
+	std::cout << "Bank Amount: $" << bankAmount << "\n";
+	std::cout << "Bank Type: " << acctType << "\n";
 
 
-
-
-
+#if 0
 
 	std::fstream thisFile;
 	thisFile.open("accountlist.txt", std::ios::app);
@@ -41,8 +78,10 @@ void openAcct() {
 		thisFile << "Good Afternoon\n";
 		thisFile.close();
 	}
+#endif
 }
 
+// 05/11/2022 - Add the "Deposit" function.
 
 
 
